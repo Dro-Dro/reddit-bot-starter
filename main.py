aad33ca91c422c06c51bad5ca5fb69476c474d21
Hakan <97197833+Dro-Dro@users.noreply.github.com>
2024-07-18 14:51:47 -0700

    import new dependencies and check if file open

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import praw
+import pdb
+import re
+import os
 
 reddit = praw.Reddit('bot1')
 subReddit = reddit.subreddit("learnpython")
+
+if not os.path.isfile("posts_replied_to.txt") :
+    posts_replied_to = []
+else :
+    with open("posts_replied_to.txt", "r") as f :
+        posts_replied_to = f.read()
+        posts_replied_to = posts_replied_to.split("\n")
+        posts_replied_to = list(filter(None, posts_replied_to))
 
 for submission in subReddit.hot(limit = 5) : 
     print("Title: ", submission.title)
